9c5b4fdc88aa68875d3b35391b99fe003c8ba0b2
root2116 <root6889@icloud.com>
2024-07-22 02:42:27 +0900

    make the job async

--- src/server.py
+++ src/server.py
@@ -1,9 +1,41 @@
 from flask import Flask, jsonify, request
 from generate import getarate_messages
 from flask_cors import CORS
+from rq import Queue
+from redis import Redis
 
 app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": "*", "methods": ["GET", "POST", "OPTIONS", "PUT", "DELETE"]}})
+
+redis_conn = Redis()
+q = Queue(connection=redis_conn)
+
+
+@app.route('/start-task', methods=['POST'])
+def start_generating():
+    job = q.enqueue(getarate_messages, "やほー！</s>", num_sentences=1, num_messages=2)
+    return jsonify({'job_id': job.get_id()}), 202
+
+@app.route('/task-status/<job_id>', methods=['GET'])
+def task_status(job_id):
+    job = q.fetch_job(job_id)
+    if job is None:
+        return jsonify({'status': 'not found'}), 404
+
+    messages = job.result
+    # 空白は改行に
+    messages = [message.replace(' ', '\n') for message in messages]
+
+    # %%%や%%は「マンボウちゃん」に置換
+    messages = [message.replace('%%%','マンボウちゃん') for message in messages]
+    messages = [message.replace('%%','マンボウちゃん') for message in messages]
+
+    # ?は「？」に置換
+    messages = [message.replace('?','？') for message in messages]
+    # !は「！」に置換
+    messages = [message.replace('!','！') for message in messages]
+
+    return jsonify({'status': job.get_status(), 'result': messages})
 
 
 @app.route('/api/messages', methods=['GET', 'OPTIONS'])
@@ -24,7 +56,7 @@ def get_messages():
     messages = [message.replace('?','？') for message in messages]
     # !は「！」に置換
     messages = [message.replace('!','！') for message in messages]
-    
+
 
     return jsonify(messages[1:])
 
