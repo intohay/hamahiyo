351a1f3ddcdc0938c6a5d4c317c10af9248fd570
root2116 <root6889@icloud.com>
2024-07-22 15:47:22 +0900

    correct db path

--- src/server.py
+++ src/server.py
@@ -15,7 +15,10 @@ load_dotenv(os.path.join(os.path.dirname(__file__), '..', '.env'))
 app = Flask(__name__)
 CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///messages.db'
+base_dir = os.path.abspath(os.path.dirname(__file__))
+db_path = os.path.join(base_dir, '..', 'messages.db')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
