c73a82d1a27411fdbf46d11649b3a4089321266a
root2116 <root6889@icloud.com>
2024-07-22 16:36:02 +0900

    bug fix

--- src/scheduler.py
+++ src/scheduler.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 
 # 上の階層にあるmessages.dbを参照するために相対パスを指定
 base_dir = os.path.abspath(os.path.dirname(__file__))
-db_path = os.path.join(base_dir, '..', 'messages.db')
+db_path = os.path.join(base_dir, '..', 'instance/messages.db')
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
