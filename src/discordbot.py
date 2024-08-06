aaebc8f9f73e0a908fd29c9689cebcab75bb5e29
root2116 <root6889@icloud.com>
2024-08-07 06:28:14 +0900

    add daily yaho

--- src/discordbot.py
+++ src/discordbot.py
@@ -1,15 +1,17 @@
 import os
 import discord
 import asyncio
-from discord.ext import commands
+from discord.ext import commands, tasks
 from dotenv import load_dotenv
 import requests
+from datetime import datetime, time, timedelta
+
 
 import aiohttp
 load_dotenv()
 
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
-# CHANNEL_ID = int(os.getenv('CHANNEL_ID'))
+CHANNEL_ID = int(os.getenv('CHANNEL_ID'))
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='/', intents=intents)
@@ -20,6 +22,7 @@ bot = commands.Bot(command_prefix='/', intents=intents)
 async def on_ready():
     print(f'Logged in as {bot.user.name}')
     await bot.tree.sync()
+    schedule_daily_yaho()
 
 
 @bot.tree.command(name='yaho', description='やほー！から始まる文章を返します')
@@ -52,6 +55,29 @@ async def generate(interaction: discord.Interaction, prompt: str):
     except Exception as e:
         # エラーハンドリング
         await interaction.followup.send(f'An error occurred: {str(e)}')
+
+
+@tasks.loop(hours=24)
+async def daily_yaho():
+    await bot.wait_until_ready()  # ボットが完全に準備されるのを待つ
+    channel = bot.get_channel(CHANNEL_ID)
+    if channel:
+        async with aiohttp.ClientSession() as session:
+            async with session.get('https://mambouchan.com/hamahiyo/generate') as response:
+                data = await response.json()
+                message = data['message']
+                message_list = message.split('[SEP]')[:3]
+                message = '\n\n'.join(message_list)
+                await channel.send(message)
+
+def schedule_daily_yaho():
+    now = datetime.now()
+    target_time = datetime.combine(now.date(), time(7, 0))
+    if now > target_time:
+        target_time += timedelta(days=1)
+    wait_time = (target_time - now).total_seconds()
+    asyncio.get_event_loop().call_later(wait_time, daily_yaho.start)
+
 
 async def main():
     await bot.start(DISCORD_TOKEN)
